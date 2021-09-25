2e581214e451c69edc92a0b1cda26f4a08da6410
Rahmouni Abdelhak <noisyboy.96.84@gmail.com>
2021-09-25 01:10:20 +0100

    Implement form validation & saving on LoginView

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,9 +1,23 @@
-// ignore_for_file: sized_box_for_whitespace, avoid_print
-
 import 'package:flutter/material.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final _formKey = GlobalKey<FormState>();
+
+	String _email = '';
+	String _password = '';
+
+  bool validateEmail(email) {
+    return RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(email);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,137 +37,167 @@ class LoginView extends StatelessWidget {
                         fit: BoxFit.cover)),
                 child: Center(
                     child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      const Image(
-                        width: 200.0,
-                        image: AssetImage('assets/images/bindex_logo.png'),
-                      ),
-                      const SizedBox(height: 80.0),
-                      TextFormField(
-                        textAlignVertical: TextAlignVertical.center,
-                        keyboardType: TextInputType.emailAddress,
-                        autofocus: false,
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 16),
-                        decoration: InputDecoration(
-                            prefixIcon: const Icon(Icons.mail_outline,
-                                color: Colors.white),
-                            hintText: 'Email',
-                            hintStyle: const TextStyle(
-                                color: Colors.white, fontSize: 16),
-                            contentPadding: const EdgeInsets.fromLTRB(
-                                20.0, 13.0, 20.0, 10.0),
-                            border: UnderlineInputBorder(
-                                borderRadius: BorderRadius.circular(32.0)),
-                            filled: true,
-                            fillColor: Colors.white.withOpacity(0.4)),
-                      ),
-                      const SizedBox(height: 12.0),
-                      TextFormField(
-                        textAlignVertical: TextAlignVertical.center,
-                        keyboardType: TextInputType.emailAddress,
-                        autofocus: false,
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 16),
-                        decoration: InputDecoration(
-                            prefixIcon: const Icon(Icons.lock_outline,
-                                color: Colors.white),
-                            hintText: 'Password',
-                            hintStyle: const TextStyle(
-                                color: Colors.white, fontSize: 16),
-                            contentPadding: const EdgeInsets.fromLTRB(
-                                20.0, 13.0, 20.0, 10.0),
-                            border: UnderlineInputBorder(
-                                borderRadius: BorderRadius.circular(32.0)),
-                            filled: true,
-                            fillColor: Colors.white.withOpacity(0.4)),
-                      ),
-                      const SizedBox(height: 6.0),
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: GestureDetector(
-                          onTap: () {
-                            print("Forgot password clicked!");
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        const Image(
+                          width: 200.0,
+                          image: AssetImage('assets/images/bindex_logo.png'),
+                        ),
+                        const SizedBox(height: 80.0),
+                        TextFormField(
+                          validator: (value) {
+                            if (value == null ||
+                                value.isEmpty ||
+                                !validateEmail(value)) {
+                              return 'Please enter a valid email address';
+                            }
+                            return null;
                           },
-                          child: const Text(
-                            "Forgot password?",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
+													onChanged: (value) {
+														_email = value;
+													},
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          textAlignVertical: TextAlignVertical.center,
+                          keyboardType: TextInputType.emailAddress,
+                          autofocus: false,
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 16),
+                          decoration: InputDecoration(
+                              errorStyle:
+                                  const TextStyle(color: Colors.orangeAccent, fontWeight: FontWeight.bold),
+                              prefixIcon: const Icon(Icons.mail_outline,
+                                  color: Colors.white),
+                              hintText: 'Email',
+                              hintStyle: const TextStyle(
+                                  color: Colors.white, fontSize: 16),
+                              contentPadding: const EdgeInsets.fromLTRB(
+                                  20.0, 13.0, 20.0, 10.0),
+                              border: UnderlineInputBorder(
+                                  borderRadius: BorderRadius.circular(32.0)),
+                              filled: true,
+                              fillColor: Colors.white.withOpacity(0.4)),
                         ),
-                      ),
-                      const SizedBox(height: 18.0),
-                      SizedBox(
-                        width: double.infinity,
-                        height: 45.0,
-                        child: ElevatedButton(
-                          onPressed: () => {},
-                          child: const Text("LOGIN"),
-                          style: ButtonStyle(
-                              shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(32.0)))),
+                        const SizedBox(height: 12.0),
+                        TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a valid password';
+                            } else if (value.length < 8) {
+                              return 'Password is at least 8 caracters long';
+                            }
+                            return null;
+                          },
+													onChanged: (value) {
+														_password = value;
+													},
+													autovalidateMode: AutovalidateMode.onUserInteraction,
+                          textAlignVertical: TextAlignVertical.center,
+                          keyboardType: TextInputType.emailAddress,
+                          autofocus: false,
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 16),
+                          decoration: InputDecoration(
+														errorStyle: const TextStyle(color: Colors.orangeAccent, fontWeight: FontWeight.bold),
+                              prefixIcon: const Icon(Icons.lock_outline,
+                                  color: Colors.white),
+                              hintText: 'Password',
+                              hintStyle: const TextStyle(
+                                  color: Colors.white, fontSize: 16),
+                              contentPadding: const EdgeInsets.fromLTRB(
+                                  20.0, 13.0, 20.0, 10.0),
+                              border: UnderlineInputBorder(
+                                  borderRadius: BorderRadius.circular(32.0)),
+                              filled: true,
+                              fillColor: Colors.white.withOpacity(0.4)),
                         ),
-                      ),
-                      const SizedBox(height: 12.0),
-                      const Text("Or login with",
-                          style: TextStyle(
-                            color: Colors.white,
-                          )),
-                      const SizedBox(height: 5.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Container(
-                            width: 100.0,
-                            child: ElevatedButton(
-                              onPressed: () => {},
-                              child: const Text("Facebook",
-                                  style: TextStyle(color: Colors.black)),
-                              style: ButtonStyle(
-                                  backgroundColor:
-                                      MaterialStateProperty.all(Colors.white),
-                                  shape: MaterialStateProperty.all<
-                                          RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(32.0)))),
+                        const SizedBox(height: 6.0),
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: const Text(
+                              "Forgot password?",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
                             ),
                           ),
-                          Container(
-                            width: 100.0,
-                            child: ElevatedButton(
-                              onPressed: () => {},
-                              child: const Text("Google",
-                                  style: TextStyle(color: Colors.black)),
-                              style: ButtonStyle(
-                                  backgroundColor:
-                                      MaterialStateProperty.all(Colors.white),
-                                  shape: MaterialStateProperty.all<
-                                          RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(32.0)))),
-                            ),
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 20.0),
-                      GestureDetector(
-                        onTap: () {
-                          print("Sign up clicked!");
-                        },
-                        child: const Text("Sign Up",
+                        ),
+                        const SizedBox(height: 18.0),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 45.0,
+                          child: ElevatedButton(
+                            onPressed: () =>
+                                {if (_formKey.currentState!.validate()) {
+																	print("email : "+_email),
+																	print("password : "+_password)
+																}},
+                            child: const Text("LOGIN"),
+                            style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(32.0)))),
+                          ),
+                        ),
+                        const SizedBox(height: 12.0),
+                        const Text("Or login with",
                             style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 25.0)),
-                      )
-                    ],
+                              color: Colors.white,
+                            )),
+                        const SizedBox(height: 5.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            SizedBox(
+                              width: 100.0,
+                              child: ElevatedButton(
+                                onPressed: () => {},
+                                child: const Text("Facebook",
+                                    style: TextStyle(color: Colors.black)),
+                                style: ButtonStyle(
+                                    backgroundColor:
+                                        MaterialStateProperty.all(Colors.white),
+                                    shape: MaterialStateProperty.all<
+                                            RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(32.0)))),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 100.0,
+                              child: ElevatedButton(
+                                onPressed: () => {},
+                                child: const Text("Google",
+                                    style: TextStyle(color: Colors.black)),
+                                style: ButtonStyle(
+                                    backgroundColor:
+                                        MaterialStateProperty.all(Colors.white),
+                                    shape: MaterialStateProperty.all<
+                                            RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(32.0)))),
+                              ),
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 20.0),
+                        GestureDetector(
+                          onTap: () {},
+                          child: const Text("Sign Up",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25.0)),
+                        )
+                      ],
+                    ),
                   ),
                 ))),
           ),
