d56a70b352c558b750179765ef2b9bca03800300
Abdelhak <rahmouni.abdelhak.1@gmail.com>
2021-11-05 01:59:08 +0100

    install & setup geolocator

--- lib/views/home/pages/explore.dart
+++ lib/views/home/pages/explore.dart
@@ -1,39 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart' as latlng;
+import '../../../services/geolocation.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Explore extends StatelessWidget {
-  const Explore({ Key? key }) : super(key: key);
+  const Explore({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    Future<Position> p = determinePosition();
+
     return FlutterMap(
       options: MapOptions(
-      center: latlng.LatLng(51.5, -0.09),
-      zoom: 13.0,
-    ),
-    layers: [
-      TileLayerOptions(
-        urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-        subdomains: ['a', 'b', 'c'],
-        attributionBuilder: (_) {
-          return const Text("© OpenStreetMap contributors");
-        },
+        center: latlng.LatLng(35.6987, 0.6349),
+        zoom: 13.0,
       ),
-      MarkerLayerOptions(
-        markers: [
-          Marker(
-            width: 80.0,
-            height: 80.0,
-            point: latlng.LatLng(51.5, -0.09),
-            builder: (ctx) =>
-            const SizedBox(
-              child: FlutterLogo(),
-            ),
-          ),
-        ],
-      ),
-    ],
+      layers: [
+        TileLayerOptions(
+          urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+          subdomains: ['a', 'b', 'c'],
+        ),
+      ],
     );
   }
 }
